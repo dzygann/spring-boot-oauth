1aff71887527efd8c18e01cdd9ea87d0a3eb7e08
Denis Zygann <denis.zygann@outlook.com>
2022-07-01 14:05:13 +0200

    Ready OAuth Demo Client App with Authorization Code Flow over GitHub
    Signed-off-by: Denis Zygann <denis.zygann@md.de>

---
+++ src/main/java/de/zygann/springbootoauth2/SpringBootOauth2Application.java
@@ -0,0 +1,16 @@
+package de.zygann.springbootoauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SpringBootOauth2Application
+{
+
+    static public void main(String[] args)
+    {
+        SpringApplication.run(SpringBootOauth2Application.class, args);
+    }
+
+}
---
+++ src/test/java/de/zygann/springbootoauth2/SpringBootOauth2ApplicationTests.java
@@ -0,0 +1,16 @@
+package de.zygann.springbootoauth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class SpringBootOauth2ApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
