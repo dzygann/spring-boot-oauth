876d85945c7bd609da623979e7e11f3a4154d4d6
Denis Zygann <denis.zygann@outlook.com>
2022-07-03 14:19:22 +0200

    Add a Logout Button

--- src/main/java/de/zygann/springbootoauth2/config/SecurityConfig.java
+++ src/main/java/de/zygann/springbootoauth2/config/SecurityConfig.java
@@ -1,15 +1,18 @@
-package de.zygann.springbootoauth2.filter;
+package de.zygann.springbootoauth2.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @EnableWebSecurity
-public class LoginSecurityFilterChain
+public class SecurityConfig
 {
 
+    @Bean
     public SecurityFilterChain loginSecurityFilterChain(HttpSecurity http) throws Exception
     {
         // antMatcher allows page request to /, /error and /webjars/**
@@ -20,6 +23,9 @@ public class LoginSecurityFilterChain
                                 .anyRequest().authenticated())
                 // default would be a redirect to login page -> the authenticationEntryPoint responds with a 401
                 .exceptionHandling(e -> e.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)))
+                .csrf(c -> c.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
+                // add logout endpoint -> spring security will clear the session and invalidate the cookie
+                .logout(l -> l.logoutSuccessUrl("/").permitAll())
                 .oauth2Login();
         return http.build();
 
